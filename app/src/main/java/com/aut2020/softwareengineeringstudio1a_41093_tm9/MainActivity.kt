1d9d1047afc8aed4621b4b56a3d2106b4bc8c3fc
Erina <erina.delinicolas@gmail.com>
2020-04-07 09:04:36 +1000

    Created Application

---
+++ app/src/main/java/com/aut2020/softwareengineeringstudio1a_41093_tm9/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aut2020.softwareengineeringstudio1a_41093_tm9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
